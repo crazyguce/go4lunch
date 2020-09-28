86b6b09c8240afab87cd26d5e4faf4a88674c8de
Bailleul Tanguy <bailleul.tanguy@gmail.com>
2020-09-28 12:02:09 +0200

    affichage map et geo

--- app/src/main/java/com/bailleul/tanguy/go4lunch/lunchfragment/MapFragment.java
+++ app/src/main/java/com/bailleul/tanguy/go4lunch/lunchfragment/MapFragment.java
@@ -1,58 +1,139 @@
 package com.bailleul.tanguy.go4lunch.lunchfragment;
 
+import android.annotation.TargetApi;
+import android.content.res.Resources;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-
 import com.bailleul.tanguy.go4lunch.R;
+import com.bailleul.tanguy.go4lunch.util.MyDateFormat;
+import com.bailleul.tanguy.go4lunch.view.DisplayNearbyPlaces;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.MapStyleOptions;
 
-public class MapFragment extends Fragment {
+import java.util.Objects;
 
-    private OnMapReadyCallback callback = new OnMapReadyCallback() {
+public class MapFragment extends Fragment implements OnMapReadyCallback, DisplayNearbyPlaces {
 
-        /**
-         * Manipulates the map once available.
-         * This callback is triggered when the map is ready to be used.
-         * This is where we can add markers or lines, add listeners or move the camera.
-         * In this case, we just add a marker near Sydney, Australia.
-         * If Google Play services is not installed on the device, the user will be prompted to
-         * install it inside the SupportMapFragment. This method will only be triggered once the
-         * user has installed Google Play services and returned to the app.
-         */
-        @Override
-        public void onMapReady(GoogleMap googleMap) {
-            LatLng sydney = new LatLng(-34, 151);
-            googleMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
-        }
-    };
+    private View mView;
 
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map, container, false);
+    private final static String TAG = "MapFragment";
+    private static final float DEFAULT_ZOOM = 16f;
+
+    //widgets
+    private ImageView mGps;
+
+    //Vars
+    private GoogleMap mMap;
+    private double myLatitude;
+    private double myLongitude;
+    private String today;
+
+    public MapFragment() {
+        // Required empty public constructor
     }
 
     @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-        SupportMapFragment mapFragment =
-                (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        if (mapFragment != null) {
-            mapFragment.getMapAsync(callback);
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        mView = inflater.inflate(R.layout.fragment_map, container, false);
+        mGps = mView.findViewById(R.id.ic_gps);
+
+        MyDateFormat forToday = new MyDateFormat();
+        today = forToday.getTodayDate();
+
+        // Default position before geolocalisation
+        myLatitude = 48.858511;
+        myLongitude = 2.294524;
+
+        return mView;
+    }
+
+
+
+    public void setUserLocation(LatLng userLatLng){
+        // update location with geolocalisation
+        myLatitude = userLatLng.latitude;
+        myLongitude = userLatLng.longitude;
+
+        if (mMap != null) {
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(myLatitude, myLongitude), DEFAULT_ZOOM));
         }
     }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initMap();
+    }
+
+    //-------------------------------------------------------------------------------------------------------------
+    // Map
+    //-------------------------------------------------------------------------------------------------------------
+    //initializing map
+    private void initMap() {
+        MapView mMapView;
+        mMapView = mView.findViewById(R.id.map);
+
+        if (mMapView != null) {
+            mMapView.onCreate(null);
+            mMapView.onResume();
+            mMapView.getMapAsync(this);
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.getUiSettings().setMyLocationButtonEnabled(false);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(myLatitude, myLongitude), DEFAULT_ZOOM));
+
+        try {
+
+            boolean success = mMap.setMapStyle(
+                    MapStyleOptions.loadRawResourceStyle(
+                            Objects.requireNonNull(getContext()), R.raw.style_json));
+
+            if (!success) {
+                Log.e(TAG, "Style parsing failed.");
+            }
+        } catch (Resources.NotFoundException e) {
+            Log.e(TAG, "Can't find style. Error: ", e);
+        }
+        init();
+    }
+
+
+    private void init() {
+        // click on gps
+        mGps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(myLatitude, myLongitude), DEFAULT_ZOOM));
+            }
+        });
+    }
+
+
 }
---
+++ app/src/main/java/com/bailleul/tanguy/go4lunch/view/DisplayNearbyPlaces.java
@@ -0,0 +1,7 @@
+package com.bailleul.tanguy.go4lunch.view;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public interface DisplayNearbyPlaces {
+    void setUserLocation(LatLng userLatLng);
+}
